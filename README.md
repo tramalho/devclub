6cb7a43f9e80488c64c2850255f861c380a62180
tramalho <thiagofacul@gmail.com>
2024-09-29 17:15:46 -0300

    config remote database connection with prisma library

--- README.md
+++ README.md
@@ -3,6 +3,20 @@
 - [GET] /users
 - [POST] /user
 
+## Database
+1 - Create account in [mongodb](https://cloud.mongodb.com)
+
+2 - Create `.env` file and add database credentials
+
+```.env
+DB_URL=mongodb+srv://<db_user>:<db_password>@freemzero.fyo28.mongodb.net/<db_name>?retryWrites=true&w=majority&appName=freeMZero
+```
+> Note: `DB_URL` **value** present in this doc is only a example. Retrive correct url connection from mongodb and set in DB_URL .env
+
+
+
 ## To Run
-> node --watch server.js
+```javascript
+node --watch server.js
+```
 
--- server.js
+++ server.js
@@ -5,6 +5,18 @@ app.use(express.json());
 
 const users = []
 
+// console.log("### process.env ###");
+// console.log(process.env.DB_USERNAME);
+// console.log(process.env.DB_PASSWORD);
+// console.log(process.env.DB_URL);
+
+// const completeUrl = process.env.DB_URL.replace("<DB_USERNAME>", process.env.DB_USERNAME)
+// .replace("<DB_PASSWORD>", process.env.DB_PASSWORD).replace("<DB_NAME>", process.env.DB_NAME);
+
+// console.log(`\n ${completeUrl}`);
+
+// console.log("###################");
+
 app.get('/users', (req, res) => {    
     res.status(200).json(users);
 });
